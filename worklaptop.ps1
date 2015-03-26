7c206cc3e51a5e518599067aef5fe6ea63dcc18f
Nathan Locke <nathan_locke@trekbikes.com>
2015-03-26 16:12:02 -0500

    fixed

---
+++ worklaptop.ps1
@@ -0,0 +1,30 @@
+#########################
+# Autoinstall script using chocolatey
+#########################
+# Note: Net 4.0 must be installed prior to running this script
+#
+#modify this line to change packages
+$items = @("GoogleChrome", "filezilla", "notepadplusplus", "foxitreader", "wireshark", "winscp", "jing", "terminals", "digsby", "vlc", "vmsphereclient", "mysql.workbench", "git", "windowsazurepowershell")
+
+#################
+# Create packages.config based on passed arguments
+#################
+$xml = '<?xml version="1.0" encoding="utf-8"?>'+ "`n" +'<packages>' + "`n"
+foreach ($item in $items)
+{
+$xml += "`t" +'<package id="' + $item + '"/>' + "`n"
+}
+$xml += '</packages>'
+
+$file = ([system.environment]::getenvironmentvariable("userprofile") + "\packages.config")
+$xml | out-File $file
+
+######
+# Install packages with cinst
+######
+
+cinst $file
+
+########
+# Delete packages.config
+Remove-Item $file
